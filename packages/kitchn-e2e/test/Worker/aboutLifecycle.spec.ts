46ff28216f07b6fd2370ba796a2b7d9bb7f67a2b
nth-commit <mfrywhite@gmail.com>
2022-11-19 11:17:11 +1300

    Add PR checks

--- packages/kitchn-e2e/test/Worker/aboutLifecycle.spec.ts
+++ packages/kitchn-e2e/test/Worker/aboutLifecycle.spec.ts
@@ -14,6 +14,7 @@ describe('Worker', () => {
     const kitchnWorker = initKitchnWorker(kitchn)
     const abortController = new AbortController()
     const { done } = await kitchnWorker.start(abortController.signal)
+    abortController.abort()
     await done
   })
 })
